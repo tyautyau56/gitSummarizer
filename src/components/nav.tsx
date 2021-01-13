c6e2ef4786a87cf59f495e63ee3bae62c5655ee9
tyautyau56 <i19yanagi@gmail.com>
2021-01-13 16:14:03 +0900

    add nav.tsx and nav.scss

---
+++ src/components/nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/nav.scss'
+
+class Nav extends React.Component<any, any> {
+  render() {
+    return (
+      <div className="nav">
+        <p>nav1</p>
+      </div>
+    )
+  }
+}
+
+export default Nav
