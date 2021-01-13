153d20c6fc1bd75e53a36991804fe65c6ad16f7f
tyautyau56 <i19yanagi@gmail.com>
2021-01-13 16:14:17 +0900

    add var.tsx and var.scss

---
+++ src/components/var.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/var.scss'
+import Nav from './nav';
+
+class Var extends React.Component<any, any> {
+  render() {
+    return (
+      <div className="var">
+        <Nav />
+      </div>
+    )
+  }
+}
+
+export default Var
