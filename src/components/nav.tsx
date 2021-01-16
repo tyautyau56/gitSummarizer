f2484c34b2453fc260bb25bebc1849cddec975bb
tyautyau56 <i19yanagi@gmail.com>
2021-01-16 23:22:41 +0900

    change nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import '../style/nav.scss'
+import '../style/nav.scss';
 
 class Nav extends React.Component<any, any> {
   render() {
     return (
-      <div className="nav">
-        <p>nav1</p>
+      <div className='nav'>
+        <div className='inner'>
+          <p>nav1</p>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Nav
+export default Nav;
